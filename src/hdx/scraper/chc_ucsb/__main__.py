ec4f41b60aaac161600d076fef01e01be6dbc800
Mike <rans@email.com>
2025-12-15 07:19:31 +1300

    HDXDSYS-2484 HDX Pipeline: CHC UCSB (#1)
    * async rsync download
    
    * Tif download only
    
    * Add tests

---
+++ src/hdx/scraper/chc_ucsb/__main__.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+"""
+Top level script. Calls other functions that generate datasets that this
+script then creates in HDX.
+
+"""
+
+import logging
+from os.path import expanduser, join
+
+from hdx.api.configuration import Configuration
+from hdx.data.resource import Resource
+from hdx.data.user import User
+from hdx.facades.infer_arguments import facade
+from hdx.utilities.downloader import Download
+from hdx.utilities.path import (
+    script_dir_plus_file,
+    wheretostart_tempdir_batch,
+)
+from hdx.utilities.retriever import Retrieve
+
+from hdx.scraper.chc_ucsb._version import __version__
+from hdx.scraper.chc_ucsb.pipeline import Pipeline
+from hdx.scraper.chc_ucsb.tiff_download import TIFFDownload
+
+# setup_logging("DEBUG")
+logger = logging.getLogger(__name__)
+
+_LOOKUP = "hdx-scraper-chc_ucsb"
+_SAVED_DATA_DIR = "saved_data"  # Keep in repo to avoid deletion in /tmp
+_UPDATED_BY_SCRIPT = "HDX Scraper: CHC UCSB"
+
+
+def create_resource_in_hdx(resource: Resource) -> None:
+    resource.create_in_hdx()
+
+
+def main(
+    save: bool = False,
+    use_saved: bool = False,
+) -> None:
+    """Generate datasets and create them in HDX
+
+    Args:
+        save (bool): Save downloaded data. Defaults to False.
+        use_saved (bool): Use saved data. Defaults to False.
+
+    Returns:
+        None
+    """
+    logger.info(f"##### {_LOOKUP} version {__version__} ####")
+    configuration = Configuration.read()
+    User.check_current_user_write_access("6e30eb6d-52f9-49de-b2cd-2d68fced05c5")
+
+    with wheretostart_tempdir_batch(folder=_LOOKUP) as info:
+        tempdir = info["folder"]
+        with Download() as downloader:
+            retriever = Retrieve(
+                downloader=downloader,
+                fallback_dir=tempdir,
+                saved_dir=_SAVED_DATA_DIR,
+                temp_dir=tempdir,
+                save=save,
+                use_saved=use_saved,
+            )
+            tiff_download = TIFFDownload(configuration, tempdir)
+            pipeline = Pipeline(tiff_download, configuration, retriever, tempdir)
+
+            for scenario in configuration["scenarios"]:
+                dataset = pipeline.generate_dataset(scenario)
+                dataset.update_from_yaml(
+                    script_dir_plus_file(
+                        join("config", "hdx_dataset_static.yaml"), main
+                    )
+                )
+                dataset.create_in_hdx(
+                    allow_no_resources=True,
+                    hxl_update=False,
+                    updated_by_script=_UPDATED_BY_SCRIPT,
+                    batch=info["batch"],
+                )
+                dataset_id = dataset["id"]
+                pipeline.add_resources(dataset_id, scenario, create_resource_in_hdx)
+
+
+if __name__ == "__main__":
+    facade(
+        main,
+        hdx_site="feature",
+        user_agent_config_yaml=join(expanduser("~"), ".useragents.yaml"),
+        user_agent_lookup=_LOOKUP,
+        project_config_yaml=script_dir_plus_file(
+            join("config", "project_configuration.yaml"), main
+        ),
+    )
---
+++ src/hdx/scraper/chc_ucsb/pipeline.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+"""Chc_ucsb scraper"""
+
+import calendar
+import logging
+from pathlib import Path
+from shutil import make_archive, rmtree
+from typing import Callable, Optional
+
+from hdx.api.configuration import Configuration
+from hdx.data.dataset import Dataset
+from hdx.data.resource import Resource
+from hdx.utilities.retriever import Retrieve
+
+from hdx.scraper.chc_ucsb.tiff_download import TIFFDownload
+
+logger = logging.getLogger(__name__)
+
+
+class Pipeline:
+    def __init__(
+        self,
+        tiff_download: TIFFDownload,
+        configuration: Configuration,
+        retriever: Retrieve,
+        tempdir: str,
+    ):
+        self._tiff_download = tiff_download
+        self._configuration = configuration
+        self._retriever = retriever
+        self._downloader = retriever.downloader
+        self._tempdir = tempdir
+        self._base_url = self._configuration["base_url"]
+        self._base_file = self._configuration["base_file"]
+
+    def generate_resource(
+        self, scenario_path: Path, scenario: str, product: str, month: int
+    ) -> Resource:
+        month_str = f"{month:02d}"
+        filename = self._base_file.format(product=product, month=month_str)
+        logger.info(f"Generating resource with {filename}.zip")
+        tif_directory = scenario_path.joinpath(product, month_str)
+        source = f"{self._base_url}/{scenario}/{month_str}"
+        tif_directory.mkdir(parents=True, exist_ok=True)
+        _ = self._tiff_download.process(source, tif_directory, include=f"*{product}*")
+        filename = self._base_file.format(product=product, month=month_str)
+        zip_path = str(scenario_path.joinpath(filename))
+        make_archive(zip_path, "zip", tif_directory)
+        zip_path = f"{zip_path}.zip"
+        month_name = calendar.month_name[month]
+        resource = Resource(
+            {
+                "name": filename,
+                "description": f"CHC-CMIP6 TMax Extremes per Country for {product} in {month_name}",
+            }
+        )
+        resource.set_format("zipped geotiff")
+        resource.set_file_to_upload(zip_path)
+        if tif_directory:
+            rmtree(tif_directory)
+        return resource
+
+    def generate_dataset(self, scenario: str) -> Optional[Dataset]:
+        start_year = self._configuration["start_year"]
+        end_year = self._configuration["end_year"]
+        dataset_name = f"chc_ucsb_tmax_{scenario.lower()}"
+        dataset_title = f"CHC-CMIP6 TMax Extremes per Country for {scenario}"
+
+        # Dataset info
+        dataset = Dataset(
+            {
+                "name": dataset_name,
+                "title": dataset_title,
+            }
+        )
+
+        dataset.set_time_period_year_range(start_year, end_year)
+        dataset.add_tags(("climate-weather", "environment"))
+        # Only if needed
+        dataset.set_subnational(False)
+        dataset.add_other_location("world")
+        return dataset
+
+    def add_resources(
+        self,
+        dataset_id: str,
+        scenario: str,
+        create_resource_in_hdx: Callable[[Resource], None],
+    ) -> None:
+        scenario_path = Path(self._tempdir, scenario)
+        scenario_path.mkdir(exist_ok=True)
+        for product in self._configuration["products"]:
+            for month in range(1, 13):
+                resource = self.generate_resource(
+                    scenario_path, scenario, product, month
+                )
+                resource["package_id"] = dataset_id
+                create_resource_in_hdx(resource)
